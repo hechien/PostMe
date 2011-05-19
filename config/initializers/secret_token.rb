399544e7e244adb0601cb4ec7b56c2de3d9fa1e8
HeChien <hechien@me.com>
2011-05-19 23:42:23 +0800

    First initialize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostMe::Application.config.secret_token = 'd7016905e69cb238b78a70183f9c3c9095e45c65939924c575d693c6fd84735cc7b846f6dee490f141a43f9d7d505abd8a40633eee22cfc593ce30a5b65056ae'
